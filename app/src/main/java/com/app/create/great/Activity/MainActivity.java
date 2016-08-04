4798c392abfec24aed4f048e246f666d2666d671
Hancher <515838965@qq.com>
2016-08-05 01:03:39 +0800

    first commit

---
+++ app/src/main/java/com/app/create/great/Activity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.app.create.great.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import com.app.create.great.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
